372587c2169bffe34dd8d59da6bf0113d8ba2a24
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-16 16:00:20 +0300

    refactor(nethttp): extract server logic into StartServer function and improve test coverage

--- server/nethttp/nethttp.go
+++ server/nethttp/nethttp.go
@@ -8,13 +8,22 @@ import (
 )
 
 func GetDatetime(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Invalid http method, only GET is allowed", http.StatusBadRequest)
+		return
+	}
+
 	t := time.Now().Format(time.RFC3339)
 	fmt.Fprint(w, t)
 }
 
-func main() {
+func StartServer() error {
 	http.HandleFunc("/datetime", GetDatetime)
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	return http.ListenAndServe(":8000", nil)
+}
+
+func main() {
+	if err := StartServer(); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
 }
--- server/nethttp/nethttp_test.go
+++ server/nethttp/nethttp_test.go
@@ -9,10 +9,30 @@ import (
 )
 
 func TestGetDatetime(t *testing.T) {
-	req := httptest.NewRequest("GET", "/datetime", nil)
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(GetDatetime)
+	t.Run("testing a GET request", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/datetime", nil)
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(GetDatetime)
 
-	handler.ServeHTTP(rr, req)
-	assert.Equal(t, http.StatusOK, rr.Code)
+		handler.ServeHTTP(rr, req)
+		assert.Equal(t, http.StatusOK, rr.Code)
+
+	})
+
+	t.Run("testing a not-GET request", func(t *testing.T) {
+		req := httptest.NewRequest("POST", "/datetime", nil)
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(GetDatetime)
+
+		handler.ServeHTTP(rr, req)
+		assert.Equal(t, http.StatusBadRequest, rr.Code)
+	})
+}
+
+func TestStartServer(t *testing.T) {
+	go func() {
+		if err := StartServer(); err != nil && err != http.ErrServerClosed {
+			t.Fatalf("error starting the server: %v", err)
+		}
+	}()
 }
